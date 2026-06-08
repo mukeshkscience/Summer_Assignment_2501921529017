bc8308dcdddcf3539c91f60f6ae3ec8d008b3a7e
mukeshkscience <mukeshk.science@gmail.com>
2026-06-08 23:20:59 +0530

    Day1

---
+++ Week_2/Day_1/FirstUniqueCharacter.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        // Count frequency of each character
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        // Find first non-repeating character
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
