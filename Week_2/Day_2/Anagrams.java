236a5c6e79e7333c1240c64ecdd2d704cd0e1d78
mukeshkscience <mukeshk.science@gmail.com>
2026-06-09 23:26:22 +0530

    Day 2

---
+++ Week_2/Day_2/Anagrams.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if (s.length() < p.length()) {
+            return result;
+        }
+
+        int[] pCount = new int[26];
+        int[] windowCount = new int[26];
+
+        // Frequency count for p and first window
+        for (int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+            windowCount[s.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(pCount, windowCount)) {
+            result.add(0);
+        }
+
+        // Slide the window
+        for (int i = p.length(); i < s.length(); i++) {
+            windowCount[s.charAt(i) - 'a']++; // add new character
+            windowCount[s.charAt(i - p.length()) - 'a']--; // remove old character
+
+            if (Arrays.equals(pCount, windowCount)) {
+                result.add(i - p.length() + 1);
+            }
+        }
+
+        return result;
+    }
+}
